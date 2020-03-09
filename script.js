2c82a01368d21aa1592d1f3a91f91edd64b10466
gggghale <falquezamiguel@gmail.com>
2020-03-09 12:38:19 +0800

    promise

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-ctr = 0;
-setInterval(function(){
-    ctr+=2;
-    document.write(`${ctr} secs has elapsed`);
-}, 2000);
+let promise = new Promise(function(res,rej){
+    grade=59;
+    if(grade>=60){
+        res("pass");
+    }else{
+        rej("fail");
+    }
+    });
+    
+    promise.then(function(message){
+    document.write(`you ${message} the subject`);
+    }).catch(function(message){
+    document.write(`you ${message} the subject`)
+    });
